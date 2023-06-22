3ac1a730e21419722916a700a30e8c0535c18654
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-22 15:08:32 +0530

    update blog information

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -4,7 +4,7 @@ export const Blogs = () => {
   useTitle('Blogs');
 
   const blogs = [{"id": 1, "name":"Function Scope", "description" : "Confused why variable you have defined is showing error? Let us learn about function scope in detail.", "jump": "https://anshulbheniye.hashnode.dev/function-scope", "image": "/assets/blog-one.jpg" },
-   {"id": 2, "name":"Coming Soon", "description" : "I will update this section as soon as I write one and I am thinking of writing one", "image": "/assets/blog-temp.jpg" }, 
+   {"id": 2, "name":"Dynamic Page Title - Custom Hooks", "description" : "Discover how to dynamically update and optimize page titles  for improved search engine visibility and better user engagement.", "jump": "https://anshulbheniye.hashnode.dev/custom-hooks-creating-a-dynamic-page-title", "image": "/assets/customHook.png" }, 
    {"id": 3, "name":"Coming Soon", "description" : "I will update this section as soon as I write one and I am thinking of writing one",  "image": "/assets/blog-temp.jpg" },];
 
   return (
